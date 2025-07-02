5247854dae671cf5dd8840be0699348d1ee7ef24
AGDevs <74154297+8NeoN8@users.noreply.github.com>
2025-07-02 19:56:49 -0400

    create accursed mechanic where spacebar incrementaly makes the cube move faster

--- index.js
+++ index.js
@@ -3,12 +3,46 @@ import {Slime} from './slime.js'
 //*DATA
 const canvas = document.getElementById('slimerun-game')
 const context = canvas.getContext('2d')
+resizeCanvas()
 
-let player = new Slime(64,64,10,10,'idle',null)
+let statesArray = [
+  {
+    type: 'idle',
+    framesTotal: 24,
+    framecount: 0
+  },
+  {
+    type: 'jumping',
+    framesTotal: 24,
+    framecount: 0
+  },
+  {
+    type: 'falling',
+    framesTotal: 4,
+    framecount: 0
+  },
+  {
+    type: 'sliding',
+    framesTotal: 4,
+    framecount: 0
+  },
+  {
+    type: 'moving',
+    framesTotal: 24,
+    framecount: 0
+  },
+  {
+    type: 'running',
+    framesTotal: 24,
+    framecount: 0
+  },
+]
+let playerSquare = 32
+let player = new Slime(playerSquare, playerSquare, 0, canvas.height - playerSquare, statesArray[0], null)
 let playerSpeedX = 6
 let playerSpeedY = 6
 
-let gravity = 3
+let gravity = 16
 
 let isGameOn = false
 
@@ -18,7 +52,8 @@ let playerMovement = {
   up: false,
   down: false,
   left: false,
-  right: false
+  right: false,
+  jump: false,
 }
 let opositesVertical = false
 let opositesHorizontal = false
@@ -36,6 +71,7 @@ function drawPlayer(){
 function resizeCanvas(){
   canvas.width =  window.innerWidth
   canvas.height = window.innerHeight - 3.0001
+  console.log(canvas.width, canvas.height);
 }
 
 function drawCanvas(){
@@ -64,20 +100,50 @@ function updatePlayer(){
   if(playerMovement.left && playerMovement.right) opositesHorizontal = true
 
   if(!opositesHorizontal){
-    if(playerMovement.left) player.posX -= playerSpeedX
-    if(playerMovement.right) player.posX += playerSpeedX
-  }
-  if(!opositesVertical){
-    if(playerMovement.up) player.posY -= playerSpeedY
-    if(playerMovement.down) player.posY += playerSpeedY
-    if(player.posY < (canvas - player.height)) player.posY += gravity
+
+    if(playerMovement.left){
+      if(isPlayerInBounds('left')){
+        player.posX = 0
+      }else player.posX -= playerSpeedX
+      
+    }
+
+    if(playerMovement.right){
+      if(isPlayerInBounds('right')){
+        player.posX = canvas.width - player.width
+      }else player.posX += playerSpeedX
+    }
   }
 
+  if(!opositesVertical){
+    if(playerMovement.up){
+      if(isPlayerInBounds('up')){
+        player.posY = 0
+      }else player.posY -= playerSpeedY
+    }
+    if(playerMovement.down){
+      if(isPlayerInBounds('down')){
+         player.posY = canvas.height - player.height
+      }else  player.posY += playerSpeedY
+    }
+    //if(player.posY < (canvas - player.height)) player.posY += gravity
+  }
 }
 
-
-//*Computed
-
+function isPlayerInBounds(bound){
+  if(bound == 'left'){
+    if(player.posX - playerSpeedX <= 0) return true
+  }
+  if(bound == 'right'){
+    if(player.posX + playerSpeedX >= canvas.width - player.width) return true
+  }
+  if(bound == 'up'){
+    if(player.posY - playerSpeedY <= 0) return true
+  }
+  if(bound == 'down'){
+    if(player.posY + playerSpeedY >= canvas.height - player.height) return true
+  }
+}
 
 //*Watch
 menu_button_start.addEventListener('click', () => {
@@ -85,13 +151,13 @@ menu_button_start.addEventListener('click', () => {
 })
 
 game_state.addEventListener('click', () => {
-  isGameOn = !isGameOn
-  enemyCollision = !enemyCollision
+  isGameOn = true
+  enemyCollision = false
   window.requestAnimationFrame(gameLoop)
 })
 
 document.addEventListener('keydown', (key) => {
-  if(key.code == "Escape"){
+  /* if(key.code == "Escape"){
     if(pause_menu.classList.contains('closed')){
       pause_menu.classList.remove('closed')
       isGameOn = false
@@ -100,41 +166,29 @@ document.addEventListener('keydown', (key) => {
       isGameOn = true
       window.requestAnimationFrame(gameLoop)
     }
-  }
+  } */
 
-  if(key.code == "KeyS"){
-    playerMovement.down = true
-  }
+  if(key.code == "KeyS") playerMovement.down = true
 
-  if(key.code == "KeyW"){
-    playerMovement.up = true
-  }
+  if(key.code == "KeyW") playerMovement.up = false
 
-  if(key.code == "KeyD"){
-    playerMovement.right = true
-  }
+  if(key.code == "KeyD") playerMovement.right = true
 
-  if(key.code == "KeyA"){
-    playerMovement.left = true
-  }
+  if(key.code == "KeyA") playerMovement.left = true
+
+  if(key.code == 'Space') playerMovement.jump = true;
 })
 
 document.addEventListener('keyup', (key) => {
-  if(key.code == "KeyS"){
-    playerMovement.down = false
-  }
+  if(key.code == "KeyS") playerMovement.down = false
 
-  if(key.code == "KeyW"){
-    playerMovement.up = false
-  }
+  if(key.code == "KeyW") playerMovement.up = false
 
-  if(key.code == "KeyD"){
-    playerMovement.right = false
-  }
+  if(key.code == "KeyD") playerMovement.right = false
 
-  if(key.code == "KeyA"){
-    playerMovement.left = false
-  }
+  if(key.code == "KeyA") playerMovement.left = false
+
+  
 })
 
 window.addEventListener('resize', () => {
@@ -142,7 +196,6 @@ window.addEventListener('resize', () => {
 })
 
 //*Run
-
 resizeCanvas()
 clearCanvas()
 drawPlayer()
